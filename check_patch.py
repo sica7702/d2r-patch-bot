27a70ccacfd9d1c0d8fe09c611faa6acacc8d07e
sica7702 <115063011+sica7702@users.noreply.github.com>
2026-06-23 02:35:06 +0900

    Update check_patch.py

--- check_patch.py
+++ check_patch.py
@@ -5,14 +5,12 @@ from bs4 import BeautifulSoup
 WEBHOOK = os.environ["DISCORD_WEBHOOK"]
 
 URL = "https://news.blizzard.com/ko-kr/diablo2"
-
 TITLE_FILE = "last_title.txt"
 
 html = requests.get(URL, timeout=30).text
-
 soup = BeautifulSoup(html, "html.parser")
 
-title = soup.title.text.strip()
+current_title = soup.title.text.strip()
 
 old_title = ""
 
@@ -20,17 +18,17 @@ if os.path.exists(TITLE_FILE):
     with open(TITLE_FILE, "r", encoding="utf-8") as f:
         old_title = f.read().strip()
 
-if title != old_title:
+if current_title != old_title:
 
     requests.post(
         WEBHOOK,
         json={
-            "content": f"📢 D2R 새 소식 감지!\n\n{title}\n{URL}"
+            "content": f"📢 D2R 새 소식 감지!\n\n{current_title}\n{URL}"
         },
         timeout=30,
     )
 
     with open(TITLE_FILE, "w", encoding="utf-8") as f:
-        f.write(title)
+        f.write(current_title)
 
 print("Done")
